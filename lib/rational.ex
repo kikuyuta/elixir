12400ea31e8979d1cc4e60f6644cb97761a8f45b
KIKUCHI, Yutaka <yu@kikuken.org>
2018-09-17 19:31:54 +0900

    major bug fix

--- lib/rational.ex
+++ lib/rational.ex
@@ -1,19 +1,30 @@
 defmodule Rational do
+
   @moduledoc """
-  A module for rational number calicuration.
-  Tupples describe rational numbers here.
+  A simple sample module for rational number calicuration.
   """
 
   @doc """
-  Hello world.
+  - Types
+  There is not new defined types.
+  Tupples describe rational numbers here.
+  The module might define a new rational type hiding numerator/denominator.
 
-  ## Examples
-
-      iex> Rational.hello()
-      :world
-
+  - Functions
+  -- add/2
+     addition of two rational numbers
+  -- sub/2
+     subtraction of two rational numbers
+  -- mul/2
+     muliplication of two rational numbers
+  -- div/2
+     division of two rational numbers
+  -- rof/1
+     Reduction of Fraction of a rational number
+  -- rcd/2
+     Reduction to Common Denominator of a list of two rational numbers.
+     Returns a list of two rational numbers whose denominators are the same.
   """
-
 
   import Kernel, except: [div: 2]
   require Integer
@@ -36,16 +47,12 @@ defmodule Rational do
     rof({n1 * d2, d1 * n2})
   end
 
-  # Reduction of Fraction
   def rof({n, d}) do
     {Kernel.div(n, Integer.gcd(n, d)), Kernel.div(d, Integer.gcd(n, d))}
   end
 
-  # Reduction to Common Denominator
   def rcd([{n1, d1}, {n2, d2}]) do
     d = d1 * d2 * Integer.gcd(d1, d2) # Least Common Muliple of d1 and d2
     [{Kernel.div(n1 * d, d1), d}, {Kernel.div(n2 * d, d2), d}]
   end
 end
-
-# numerator/denominator
